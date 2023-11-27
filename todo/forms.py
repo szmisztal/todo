b3165fee99acb8ec4790475ae7206a156133d453
szmisztal <sz.misztal@gmail.com>
2023-11-27 17:15:04 +0100

    remake repo

--- todo/forms.py
+++
@@ -1,8 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import StringField, FloatField, BooleanField
-from wtforms.validators import DataRequired
-
-class TodoForm(FlaskForm):
-    expense = StringField('Expense', validators = [DataRequired()])
-    cost = FloatField('Cost', validators = [DataRequired()])
-    paid = BooleanField('Paid', default = False)
